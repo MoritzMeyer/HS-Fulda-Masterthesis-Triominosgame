cb56bd2c002fcf3d448fe2c6e39dbc3947f717dc
MoritzMeyer <privat.moritzmeyer@googlemail.com>
2019-08-29 19:55:43 +0200

    added function to draw tiles from stack to player drawboard

---
+++ Triominos Board Game/Assets/Scripts/DrawBoardManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBoardManager : MonoBehaviour
+{
+    public float TilePositionY = 5.0f;
+    public float TilePositionZ = -2f;
+    public float TileMaxPositionX = 0.32f;
+    public float TileMinPositionX = -0.32f;
+    public float TileOffsetX = 0.08f;
+
+    public void RemoveTile(GameObject tile)
+    {
+        tile.transform.parent = null;
+        tile.layer = 0;
+        this.ReArrangeTiles();        
+    }
+
+    public void AddTile(GameObject tile)
+    {
+        tile.transform.parent = this.transform;
+        tile.layer = this.gameObject.layer;
+        this.ReArrangeTiles();
+    }
+
+    public void ReArrangeTiles()
+    {
+        float startPositionX = 0 - ((this.transform.childCount - 1) * this.TileOffsetX / 2);
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            float positionX = startPositionX + (i * TileOffsetX);
+            Transform child = this.transform.GetChild(i);
+            child.localPosition = new Vector3(positionX, TilePositionY, TilePositionZ);
+        }
+    }
+}
---
+++ Triominos Board Game/Assets/Scripts/RandomTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTile : MonoBehaviour
+{
+    public GameObject Tiles;
+    public GameObject DrawBoardPlayer1;
+    public GameObject DrawBoardPlayer2;
+
+    public void PlaceRandomTile()
+    {
+        int randomIndex = Random.Range(0, Tiles.transform.childCount);
+        GameObject randomTile = Tiles.transform.GetChild(randomIndex).gameObject;
+        randomTile.gameObject.SetActive(true);
+        DrawBoardPlayer2.GetComponent<DrawBoardManager>().AddTile(randomTile);
+    }
+}
